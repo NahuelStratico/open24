d7321febd219f9754a0f41ece27c5dc9cf6ac84d
Nahuel <49825382+NahuelStratico@users.noreply.github.com>
2020-12-06 13:09:10 -0300

    iniciando con el navbar y el responsive

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './app.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './component/general/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/general/Navbar.jsx
@@ -0,0 +1,33 @@
+import '../../component/general/navbar.css'
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container">
+                <a className="navbar-brand" href="#">Open 24/7</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Alfajores</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Snacks</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Bebidas</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
